bfe76ac3dcd46e0c8a196c8f86a3b36614e24cb1
monokrohm <raymond.g.fung@gmail.com>
2023-11-14 12:25:33 +1100

    first commit

--- src/InputCheck.java
+++ src/InputCheck.java
@@ -2,27 +2,25 @@ import java.util.Scanner;
 
 /**
  * This class is used to check if the user input is of the correct data type
+ * 
  * @author Raymond Fung
  * @version ver 1.0.0
  */
-public class InputCheck
-{
+public class InputCheck {
     /**
      * Default constructor which creates the object of the class InputCheck.
      *
      */
-    public InputCheck()
-    {
+    public InputCheck() {
     }
 
     /**
      * Checks if the user input can be returned as a string type
      *
-     * @param    prompt    Accepts a string message asking for input
-     * @return             The input from the user as a string
+     * @param prompt Accepts a string message asking for input
+     * @return The input from the user as a string
      */
-    public String strInput(String prompt)
-    {
+    public String strInput(String prompt) {
         Scanner console = new Scanner(System.in);
         System.out.println(prompt);
         return console.nextLine();
@@ -31,11 +29,10 @@ public class InputCheck
     /**
      * Checks if the user input can be returned as an integer type
      *
-     * @param    prompt    Accepts a string message asking for input
-     * @return             The input from the user as an integer
+     * @param prompt Accepts a string message asking for input
+     * @return The input from the user as an integer
      */
-    public int intInput(String prompt)
-    {
+    public int intInput(String prompt) {
         Scanner console = new Scanner(System.in);
         System.out.println(prompt);
         return Integer.parseInt(console.nextLine().trim());
